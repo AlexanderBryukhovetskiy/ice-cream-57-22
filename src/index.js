18e1335b50ed0aca0eed910d9543b63871a64eeb
Gitforcommit <artemenko.vladyslav@gmail.com>
2022-07-24 02:40:36 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-$(document).ready(function () {
-  $('.slider').slick();
+$('.single-item').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
 });
-
-// $('.single-item').slick({
-//   infinite: true,
-//   dots: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-// });
