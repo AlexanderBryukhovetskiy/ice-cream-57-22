bc8e55e775894b2a864096bbf66efb3b52482e40
AlexanderBryukhovetskiy <105162184+AlexanderBryukhovetskiy@users.noreply.github.com>
2022-07-26 21:39:44 +0300

    Revert "Customer reviews for fetch"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-// $('.single-item').slick({
-//   infinite: true,
-//   dots: true,
-//   slidesToShow: 1,
-//   slidesToScroll: 1,
-// });
-
-const { Pagination } = require('swiper');
-
-// });
-new Swiper('.slider', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+$('.single-item').slick({
+  infinite: true,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
 });
