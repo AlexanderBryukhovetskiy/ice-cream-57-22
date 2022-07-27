054bce1d23ad0de703b373fe5722075070ae621a
AlexanderBryukhovetskiy <105162184+AlexanderBryukhovetskiy@users.noreply.github.com>
2022-07-27 19:24:56 +0000

    Deploying to gh-pages from @ AlexanderBryukhovetskiy/ice-cream-57-22@48d72af798420c54445e11a239a64d0ca6a728a0 🚀

--- index.327e8a70.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".slider",{pagination:{el:".swiper-pagination",clickable:!0}});
-//# sourceMappingURL=index.327e8a70.js.map
---
+++ index.d213d768.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{pagination:{el:".swiper-pagination",clickable:!0,Boolean:!0,autoplay:{delay:10}}});
+//# sourceMappingURL=index.d213d768.js.map
