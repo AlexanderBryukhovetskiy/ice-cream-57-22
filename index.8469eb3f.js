d735b5a3a29d79b0637058d53a6af49fdb79f5c9
AlexanderBryukhovetskiy <105162184+AlexanderBryukhovetskiy@users.noreply.github.com>
2024-01-04 10:51:18 +0000

    Deploying to gh-pages from @ AlexanderBryukhovetskiy/ice-cream-57-22@2b47fb9c2cd88a408a00a735758f936a27772516 🚀

---
+++ index.8469eb3f.js
@@ -0,0 +1,2 @@
+new Swiper(".slider",{pagination:{el:".swiper-pagination",clickable:!0,Boolean:!0,autoplay:{delay:10}}});
+//# sourceMappingURL=index.8469eb3f.js.map
---
+++ index.eb7f6862.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector(".activity-button"),closeModalBtn:document.querySelector(".about-modal-close-btn"),modal:document.querySelector("[prod-modal-cream]")};function o(){e.modal.classList.toggle("prod-cream")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.eb7f6862.js.map
